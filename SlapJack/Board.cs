3631a41a987f87efb3c86037eaa1c916922d039c
Karanpreet Singh <44882552+ksingh94@users.noreply.github.com>
2019-02-08 00:26:15 -0700

    Add files via upload

---
+++ SlapJack/Board.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slapjack
+{
+    public class Board
+
+    {
+        private static int MAX_PLAYERS = 2;
+        private List<Card> pile;
+        public List<Player> players { get; }
+        private Deck deck;
+        public int lastPlayed { get; set; }
+
+
+        public Card getTopCard()
+        {
+            try
+            {
+                return pile[0];
+            }
+
+            catch (ArgumentOutOfRangeException)
+
+            {
+                Console.WriteLine("There are no cards on the pile.");
+
+            }
+
+            return null;
+        }
+
+        public int getPileCount()
+
+        {
+            return pile.Count;
+
+        }
+
+        public Board()
+
+        {
+            //instantiate objects needed for game
+
+            pile = new List<Card>();
+
+            players = new List<Player>();
+
+            for (int playerId = 0; playerId < MAX_PLAYERS; playerId++)
+
+                players.Add(new Player());
+
+            lastPlayed = 2;
+
+            //deal players their cards
+
+            deck = new Deck();
+
+            Card nextCard = deck.deal();
+
+            while (nextCard != null)
+
+            {
+                for (int playerId = 0; playerId < MAX_PLAYERS; playerId++)
+
+                {
+                    players[playerId].receiveCard(nextCard);
+
+                    nextCard = deck.deal();
+                }
+            }
+        }
+        //get a player
+        //used by SlapJack to call methods on the player
+
+        public Player getPlayer(int playerNum)
+
+        {
+            return players[playerNum - 1];
+
+        }
+
+        public void addCard(Card newCard)
+
+        {
+            if (newCard != null)
+
+                pile.Insert(0, newCard);
+        }
+
+
+        public bool playerSlapped(int playerId)
+
+        {
+            bool slapValidity = isValidSlap();
+
+            if (slapValidity)
+
+            {
+                getPlayer(playerId).addToBottom(pile);
+
+                Console.WriteLine("Player " + playerId + "'s slap succeded! He added " + pile.Count + " cards to his hand");
+
+                pile.Clear();
+            }
+
+            else
+
+            {
+                Console.WriteLine("Player " + playerId + "'s slap failed. He gave a card to player " + (3 - playerId));
+
+                getPlayer((3 - playerId)).receiveCard(getPlayer(playerId).Flip());
+
+            }
+
+            return slapValidity;
+        }
+
+        public Card playerFlipped(int playerId)
+
+        {
+            if (playerId != lastPlayed)
+
+            {
+                Card card = getPlayer(playerId).Flip();
+
+                if (card == null)
+
+                    return null;
+
+                //addCard(card);
+
+                lastPlayed = playerId;
+
+                return card;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+
+        /// Checks each players hand for a winner
+
+        /// </summary>
+
+        /// <returns>-1 if there is no winner or the id of the player that won</returns>
+
+        public int checkWinner()
+
+        {
+            int winnerId = -1;
+
+            for (int playerId = 0; playerId < MAX_PLAYERS; playerId++)
+
+            {
+
+                winnerId = players[playerId].getHandCount() >= 52 ? playerId : winnerId;
+
+            }
+
+            for (int playerId = 0; playerId < MAX_PLAYERS; playerId++)
+            {
+
+                winnerId = (players[playerId].getHandCount() + pile.Count) >= 52 ? playerId : winnerId;
+            }
+
+            return winnerId;
+        }
+        public Boolean isValidSlap()
+
+        {
+            try
+
+            {
+                return getTopCard().faceValue == Face.Jack;
+            }
+            catch (NullReferenceException)
+
+            {
+                return false;
+            }
+        }
+    }
+}
