de4e1b91ae9ef476a1cc05b5d2d67fa43d392e8e
Wesley Hooker <wesleyhooker@mail.weber.edu>
2019-02-11 10:16:23 -0700

    UI Methods
    Added Bored class. Added UI methods

--- SlapJack/MainWindow.xaml.cs
+++ SlapJack/MainWindow.xaml.cs
@@ -25,24 +25,58 @@ namespace SlapJack
         /// <summary>
         /// Keeps Track of inGame Time
         /// </summary>
-        Timer timer;
+        static Timer roundTimer;
+        /// <summary>
+        /// Game Logic
+        /// </summary>
+        Bored slapJack;
         #endregion
 
 
-
-        #region METHODS
+        #region MAIN METHODS
         public MainWindow()
         {
             InitializeComponent();
+        }
+        #endregion
 
-            // Create a timer with a two second interval.
-            timer = new System.Timers.Timer(2000);
-            // Hook up the Elapsed event for the timer. 
-            //timer.Elapsed += OnTimedEvent; The function to be called when 2 seconds finishes
-            timer.AutoReset = true;
-            timer.Enabled = true;
+
+        #region UI METHODS
+        /// <summary>
+        /// Starts a new SlapJack game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            //Allow Buttons to be pressed
+            imgPlayerCard.IsEnabled = true;
+            canImgPile.IsEnabled = true;
+            slapJack = new Bored();
+
+            //Reset timer
+            if (roundTimer != null)
+            {
+                roundTimer.Stop();
+                roundTimer.Dispose();
+            }
         }
 
+        /// <summary>
+        /// Players Hand is clicked. Places top card on pile.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImgPlayerCard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Highlights the pile for slapping
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CanImgPile_MouseEnter(object sender, MouseEventArgs e)
         {
             //Highlight the Card
@@ -51,12 +85,39 @@ namespace SlapJack
             imgPile.Opacity = 0.9;
         }
 
+        /// <summary>
+        /// Unhighlights the pile for slapping
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CanImgPile_MouseLeave(object sender, MouseEventArgs e)
         {
             //UnHighlight the Card
             borderImgPile.BorderBrush = null;
             txtImgPile.Visibility = Visibility.Hidden;
             imgPile.Opacity = 1;
+        }
+        #endregion
+
+
+        #region TIMER METHODS
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            roundTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            roundTimer.Elapsed += OnTimedEvent;
+            roundTimer.AutoReset = true;
+            roundTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Play the card
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
         }
         #endregion
     }
