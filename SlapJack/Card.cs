980e41aa37c26edb934fb159aac185bb7e370e3f
Brandon Burke <brandonburke93@outlook.com>
2019-02-13 10:05:02 -0700

    Update Card.cs

--- SlapJack/Card.cs
+++ SlapJack/Card.cs
@@ -5,16 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SlapJack
-{
-    public enum Face {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
-    
+{    
     class Card
     {
         public string Suit { get; set; }
         public string Face { get; set; }
-        private Face face;
-       
-        
 
         public Card(string cardSuit, string cardFace)
         {
@@ -25,17 +20,6 @@ namespace SlapJack
         public override string ToString()
         {
             return (Face + "_of_" + Suit + ".png");
-        }
-
-        public Face faceValue
-        {
-            get
-            { return face;
-            }
-            set
-            { face = value;
-            }
-
         }
     }
 }
